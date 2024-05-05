ef26d02ea4360b552738e535f035d63a3d6bff8a
Shahnoza <otakulovashahnoza@gmail.com>
2024-05-05 12:11:17 -0400

    I made a change

---
+++ Changes.java
@@ -0,0 +1,6 @@
+public class Changes {
+     public static void main(String[] args) {
+         System.out.println("I am learning git");
+         System.out.println("I am learning advanced git");
+    }
+}
--- Demo.java
+++ Demo.java
@@ -1,8 +1,8 @@
 public class Demo {
-
-    public static void main(String[] args) {
-        System.out.println("Version 1");
-
-        System.out.println("Version 2");
-    }
+//
+//    public static void main(String[] args) {
+////        System.out.println("Version 1");
+////
+////        System.out.println("Version 2");
+//    }
 }
